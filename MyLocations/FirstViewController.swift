e1e96272cc0d4c131a7e94c13b5b047ebd7b2608
Jeffery Trotz <jeff@Jeffreys-iMac.local>
2019-03-20 15:22:16 -0400

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Jeffery Trotz on 3/20/19.
+//  Copyright © 2019 Jeffery Trotz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
